5f1bff847aac9c3383ecd541c15e23512830837c
romannvz <romannvz@ya.ru>
2024-04-03 21:36:43 +0500

    first commit before autotests

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,23 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const container = document.querySelector(".places__list");
+const temp = document.querySelector("#card-template").content;
+
+function createCard(card, def) {
+  const inTemp = temp.querySelector(".places__item").cloneNode(true);
+  inTemp.querySelector(".card__image").src = card.link;
+  inTemp.querySelector(".card__title").textContent = card.name;
+  const deleteButton = inTemp.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", def);
+  return inTemp;
+}
+
+function deleteCard(event) {
+  event.target.closest(".places__item").remove();
+}
+
+initialCards.forEach((item) => {
+  container.append(createCard(item, deleteCard));
+});
